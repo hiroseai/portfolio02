9a9f65dca10cf25098475a2ee4cc5a36f65e13f8
hiroseai <h7tm2a@outlook.jp>
2026-08-13 21:13:39 +0900

    first

--- js/main.js
+++ js/main.js
@@ -2,34 +2,20 @@
 header > hamburger-menu
 ------------------------------------------ */
 const hamburger = $('.js-header__hamburger-menu');
-const body = $('.js-body'); // ★bodyタグを操作するために追加
+const body = $('.js-body');
 
 $('.js-header__hamburger-button').on('click', function () {
     hamburger.toggleClass('js-header__hamburger-menu-active');
-    body.toggleClass('is-overflow'); // ★メニューの開閉に合わせてbodyの固定を切り替え
+    body.toggleClass('is-overflow');
 });
 
 $(window).on('resize', function () {
     hamburger.removeClass('js-header__hamburger-menu-active');
-    body.removeClass('is-overflow'); // ★bodyの固定を強制解除
+    body.removeClass('is-overflow');
 });
 
 $('.js-header__hamburger-menu a[href^="#"]').on('click', function () {
     hamburger.removeClass('js-header__hamburger-menu-active');
-    body.removeClass('is-overflow'); // ★ページ内スクロールが始まる前にbodyの固定を解除
+    body.removeClass('is-overflow');
 });
 
-/*
-header > transform
------------------------------------------- */
-$(function () {
-    $(window).on("scroll", function () {
-        const headerHeight = $(".js-header").outerHeight();
-        const kvHeight = $(".js-kv").height();
-        if ($(this).scrollTop() >= kvHeight - headerHeight) {
-            $(".js-header").addClass("transform");
-        } else {
-            $(".js-header").removeClass("transform");
-        }
-    });
-});
