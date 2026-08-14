25101ea1d6c8deb64590a88fda5f53e556fd33f3
hiroseai <h7tm2a@outlook.jp>
2026-08-14 18:10:41 +0900

    first

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 /*
 header > hamburger-menu
 ------------------------------------------ */
-const hamburger = $('.js-header__hamburger-menu');
-const body = $('.js-body');
+$(function () {
+    const $hamburger = $('.js-header__hamburger-menu');
+    const $body = $('.js-body');
 
-$('.js-header__hamburger-button').on('click', function () {
-    hamburger.toggleClass('js-header__hamburger-menu-active');
-    body.toggleClass('is-overflow');
+    $('.js-header__hamburger-button').on('click', function () {
+        $hamburger.toggleClass('js-header__hamburger-menu-active');
+        $body.toggleClass('is-overflow');
+    });
+
+    $(window).on('resize', function () {
+        $hamburger.removeClass('js-header__hamburger-menu-active');
+        $body.removeClass('is-overflow');
+    });
+
+    $('.js-header__hamburger-menu a[href^="#"]').on('click', function () {
+        $hamburger.removeClass('js-header__hamburger-menu-active');
+        $body.removeClass('is-overflow');
+    });
 });
-
-$(window).on('resize', function () {
-    hamburger.removeClass('js-header__hamburger-menu-active');
-    body.removeClass('is-overflow');
-});
-
-$('.js-header__hamburger-menu a[href^="#"]').on('click', function () {
-    hamburger.removeClass('js-header__hamburger-menu-active');
-    body.removeClass('is-overflow');
-});
-
